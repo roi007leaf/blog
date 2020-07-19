72b03950cbba4c8b195437d5efb0f337dc04a705
roi.horowitz <roi.horowitz@payboxapp.com>
2020-07-19 07:45:41 +0300

    Feature: add k8s cluster ip service for the rest of the services Fix: event bus deployment k8s container name

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,12 +7,13 @@ app.use(bodyParser.json());
 
 const events = [];
 app.post('/events', (req, res) => {
+    console.log(`Received Event ${req.body.type}`)
     const event = req.body;
     events.push(event);
     axios.post('http://posts-clusterip-srv:4000/events', event);
-    // axios.post('http://localhost:4001/events', event);
-    // axios.post('http://localhost:4002/events', event);
-    // axios.post('http://localhost:4003/events', event);
+    axios.post('http://comments-clusterip-srv:4001/events', event);
+    axios.post('http://query-clusterip-srv:4002/events', event);
+    axios.post('http://moderation-clusterip-srv:4003/events', event);
 
     res.send({status: "OK"})
 
--- moderation/index.js
+++ moderation/index.js
@@ -8,7 +8,7 @@ app.post('/events', (req, res) => {
     const {type, data} = req.body;
     if (type === "CommentCreated") {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
-        axios.post('http://localhost:4005/events',
+        axios.post('http://event-bus-srv:4005/events',
             {
                 type: 'CommentModerated',
                 data: {
